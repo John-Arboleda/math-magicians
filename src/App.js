61902ce8a5866034de5bff8264fb1ad4bbe2c3f1
John-Arboleda <johnjack0126@gmail.com>
2022-04-21 18:28:11 -0500

    Fix linter offenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable class-methods-use-this */
 import React from 'react';
 import './App.css';
+/* eslint-disable-next-line no-unused-vars */
 import Calculator from './components/calculator';
 
 class App extends React.Component {
