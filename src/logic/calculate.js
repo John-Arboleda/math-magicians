7052b0e15cf6cef8f3fd1a8328aea6e7a6bbc72b
John-Arboleda <johnjack0126@gmail.com>
2022-04-22 12:41:52 -0500

    Improve display of results, fix AC button

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -16,9 +16,9 @@ function isNumber(item) {
 export default function calculate(obj, buttonName) {
   if (buttonName === 'AC') {
     return {
-      total: null,
-      next: null,
-      operation: null,
+      total: '',
+      next: '',
+      operation: '',
     };
   }
 
@@ -70,7 +70,7 @@ export default function calculate(obj, buttonName) {
       return {
         total: operate(obj.total, obj.next, obj.operation),
         next: null,
-        operation: null,
+        operation: '',
       };
     }
     // '=' with no operation, nothing to do
